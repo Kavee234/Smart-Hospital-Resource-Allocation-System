50c726ca6bdbf3f0e174f39c2eb0da137dd6033f
Kavee234 <kaveeshap81@gmail.com>
2026-09-17 12:27:44 +0530

    Commit 05: Defined specialty and ward lookup constant arrays

--- hospital.c
+++ hospital.c
@@ -2,3 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hospital.h"
+
+const char SPECIALTY_NAMES[NUM_SPECIALTIES][30] = {"General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"};
+const double BASE_FEES[NUM_SPECIALTIES] = {1500.00, 2500.00, 4500.00, 5000.00};
+const int CONSULT_TIMES[NUM_SPECIALTIES] = {15, 20, 30, 30};
+const int DAILY_CAPS[NUM_SPECIALTIES] = {30, 20, 12, 10};
+
+const char WARD_NAMES[NUM_WARDS][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
+const double WARD_RATES[NUM_WARDS] = {3000.00, 6000.00, 12000.00, 25000.00};
+const int WARD_CAPACITIES[NUM_WARDS] = {20, 10, 10, 5};
