8bd5165f212bdd86dc8600396aea585c78031fc2
Kavee234 <kaveeshap81@gmail.com>
2026-09-17 20:14:24 +0530

    Commit 08: Implemented registerPatient routine using parallel arrays with easy to use interface

--- hospital.c
+++ hospital.c
@@ -36,3 +36,63 @@ void initializeSystem(int bedOccupancy[NUM_WARDS][20], int queueCounts[NUM_SPECI
         queueCounts[i] = 0;
     }
 }
+
+void registerPatient(char names[][50], int ages[], int urgencies[], int specIDs[],
+                     int wardAdmitted[], int wardIDs[], int daysAdmitted[],
+                     double finalPayables[], int bedOccupancy[NUM_WARDS][20],
+                     int queueCounts[NUM_SPECIALTIES], int *patientCount) {
+    if (*patientCount >= MAX_PATIENTS) {
+        printf("Error: Patient database full!\n");
+        return;
+    }
+
+    int idx = *patientCount;
+    printf("\n====================================\n");
+    printf("-----   Patient Registration   -----\n");
+    printf("====================================\n");
+    printf("Enter Patient Name: ");
+    scanf(" %[^\n]", names[idx]);
+    printf("Enter Age: ");
+    scanf("%d", &ages[idx]);
+    printf("\n---  Urgency Levels  ---\n");
+    printf(" Normal   = 1\n");
+    printf(" Urgent   = 2\n");
+    printf(" Critical = 3\n");
+    printf("Enter Urgency Level: ");
+    scanf("%d", &urgencies[idx]);
+    printf("\n-----   Specialties   -----\n");
+    printf(" General Practice(OPD) = 1\n");
+    printf(" Paediatrics           = 2\n");
+    printf(" Cardiology            = 3\n");
+    printf(" Neurology             = 4\n");
+    printf("Select Specialty: ");
+    scanf("%d", &specIDs[idx]);
+
+    printf("\n---  Admitted to Ward  ---\n");
+    printf(" Yes = 1\n");
+    printf(" No  = 0\n");
+    printf("Enter Option: ");
+    scanf("%d", &wardAdmitted[idx]);
+
+    if (wardAdmitted[idx] == 1) {
+        printf("\n---   Wards List   ---\n");
+        printf(" General    = 1\n");
+        printf(" Paediatric = 2\n");
+        printf(" Surgical   = 3\n");
+        printf(" ICU        = 4\n");
+        printf("Enter Ward: ");
+        scanf("%d", &wardIDs[idx]);
+        printf("Enter Days Admitted: ");
+        scanf("%d", &daysAdmitted[idx]);
+
+        int wIdx = wardIDs[idx] - 1;
+        for (int b = 0; b < WARD_CAPACITIES[wIdx]; b++) {
+            if (bedOccupancy[wIdx][b] == 0) {
+                bedOccupancy[wIdx][b] = 1;
+                break;
+            }
+        }
+    } else {
+        wardIDs[idx] = 0;
+        daysAdmitted[idx] = 0;
+    }
