5ad845bd46d07b351f92a81e5f890b61ccb32350
Kavee234 <kaveeshap81@gmail.com>
2026-09-18 11:54:27 +0530

    Commit 12: Added formal billing invoice layout matching assignment requirements

--- hospital.c
+++ hospital.c
@@ -110,3 +110,43 @@ double discount = (ages[idx] < 5 || ages[idx] > 65) ? (gross * 0.15) : 0.0;
     queueCounts[sIdx]++;
     (*patientCount)++;
 }
+
+void displayAllBills(char names[][50], int ages[], int urgencies[], int specIDs[],
+                     int wardAdmitted[], int wardIDs[], int daysAdmitted[],
+                     double finalPayables[], int queueCounts[NUM_SPECIALTIES], int patientCount) {
+    if (patientCount == 0) {
+        printf("No patients registered yet.\n");
+        return;
+    }
+    for (int i = 0; i < patientCount; i++) {
+        int sIdx = specIDs[i] - 1;
+        double baseFee = BASE_FEES[sIdx];
+        double surchargeRate = (urgencies[i] == 1) ? 0.0 : (urgencies[i] == 2) ? 0.20 : 0.50;
+        double surcharge = baseFee * surchargeRate;
+        double wardCost = (wardAdmitted[i] == 1) ? (daysAdmitted[i] * WARD_RATES[wardIDs[i] - 1]) : 0.0;
+        double gross = baseFee + surcharge + wardCost;
+        double discount = (ages[i] < 5 || ages[i] > 65) ? (gross * 0.15) : 0.0;
+        int waitTime = (urgencies[i] == 3) ? 0 : (queueCounts[sIdx] * CONSULT_TIMES[sIdx]);
+
+        printf("\n===================================================\n");
+        printf("          SMART HOSPITAL ADMISSION & BILL          \n");
+        printf("---------------------------------------------------\n");
+        printf("Patient ID             : PAT-%d\n", 1001 + i);
+        printf("Patient Name           : %s\n", names[i]);
+        printf("Age                    : %d Years %s\n", ages[i], (ages[i] < 5 || ages[i] > 65) ? "(15% Subsidy Eligible)" : "");
+        printf("Specialty              : %s\n", SPECIALTY_NAMES[sIdx]);
+        printf("Assigned Ward          : %s\n", (wardAdmitted[i] == 1) ? WARD_NAMES[wardIDs[i] - 1] : "Outpatient (OPD)");
+        printf("Urgency Level          : Level %d\n", urgencies[i]);
+        printf("---------------------------------------------------\n");
+        printf("Base Consultation Fee  : LKR %.2f\n", baseFee);
+        printf("Emergency Surcharge    : LKR %.2f\n", surcharge);
+        printf("Ward Stay Cost         : LKR %.2f\n", wardCost);
+        printf("---------------------------------------------------\n");
+        printf("Gross Total Bill       : LKR %.2f\n", gross);
+        printf("Age Subsidy Discount   : LKR -%.2f\n", discount);
+        printf("---------------------------------------------------\n");
+        printf("Final Payable Amount   : LKR %.2f\n", finalPayables[i]);
+        printf("Estimated Wait Time    : %d mins\n", waitTime);
+        printf("===================================================\n");
+    }
+}
